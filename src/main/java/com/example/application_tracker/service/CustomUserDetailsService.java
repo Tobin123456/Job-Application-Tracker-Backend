1bede5f40f0f04584eb4a9100937ed10a0be9b1a
Tobin Zühlke <zuhlketobin@gmail.com>
2026-01-04 21:43:37 +0100

    Added UserDetailsService which loads authentification and authorization information of a user from the database

---
+++ src/main/java/com/example/application_tracker/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.application_tracker.service;
+
+import com.example.application_tracker.domain.User;
+import com.example.application_tracker.repository.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @NonNull
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRoles().toArray(new String[0]))
+                .build();
+    }
+}
